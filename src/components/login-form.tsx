27840204bcf8aff62e3731648212322a1155af3a
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-20 22:16:12 +0600

    feat: login page design done

---
+++ src/components/login-form.tsx
@@ -0,0 +1,54 @@
+import {
+  EnvelopeClosedIcon,
+  LockClosedIcon,
+  StackIcon,
+} from "@radix-ui/react-icons";
+import { Button, TextField } from "@radix-ui/themes";
+import React from "react";
+
+export default function LoginForm() {
+  return (
+    <section className="h-screen w-full flex flex-col justify-center gap-6 max-w-md mx-auto px-4">
+      <span className="flex flex-col items-start gap-2">
+        <StackIcon height="32" width="32" />
+        <h1 className="text-2xl font-semibold tracking-tighter">
+          Welcome Back!
+        </h1>
+      </span>
+
+      <TextField.Root
+        placeholder="Enter your email"
+        type="email"
+        color="gray"
+        size="3"
+        variant="soft"
+        radius="large"
+      >
+        <TextField.Slot>
+          <EnvelopeClosedIcon height="16" width="16" />
+        </TextField.Slot>
+      </TextField.Root>
+      <TextField.Root
+        placeholder="Enter your password"
+        type="password"
+        color="gray"
+        size="3"
+        variant="soft"
+        radius="large"
+      >
+        <TextField.Slot>
+          <LockClosedIcon height="16" width="16" />
+        </TextField.Slot>
+      </TextField.Root>
+      <Button
+        color="gray"
+        size="3"
+        variant="classic"
+        highContrast
+        radius="large"
+      >
+        Login
+      </Button>
+    </section>
+  );
+}
