716e152d9c3bdf6fef4fdae35a00df801ba27b23
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-20 22:48:03 +0600

    feat: update login form

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,57 +1,68 @@
+"use client";
+
 import {
   EnvelopeClosedIcon,
   LockClosedIcon,
   StackIcon,
 } from "@radix-ui/react-icons";
 import { Button, TextField } from "@radix-ui/themes";
-import React from "react";
+import React, { FormEvent } from "react";
 
 export default function LoginForm() {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
-    <section className="h-screen w-full flex flex-col justify-center gap-6 max-w-md mx-auto px-4">
-      <span className="flex flex-col items-start gap-2">
-        <StackIcon height="32" width="32" />
-        <h1 className="text-2xl font-semibold tracking-tighter">
-          Welcome Back!
-        </h1>
-      </span>
-
-      {/* email field */}
-      <TextField.Root
-        placeholder="Enter your email"
-        type="email"
-        color="gray"
-        size="3"
-        variant="soft"
-        radius="large"
-      >
-        <TextField.Slot>
-          <EnvelopeClosedIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
-      {/* password field */}
-      <TextField.Root
-        placeholder="Enter your password"
-        type="password"
-        color="gray"
-        size="3"
-        variant="soft"
-        radius="large"
-      >
-        <TextField.Slot>
-          <LockClosedIcon height="16" width="16" />
-        </TextField.Slot>
-      </TextField.Root>
-      <Button
-        color="gray"
-        size="3"
-        variant="classic"
-        radius="large"
-        highContrast
-        className="hover:cursor-pointer"
-      >
-        Login
-      </Button>
+    <section className="h-screen w-full grid place-items-center px-4">
+      <div className="w-full max-w-md mx-auto flex flex-col justify-center gap-12">
+        <span className="flex flex-col items-start gap-2">
+          <StackIcon height="32" width="32" />
+          <h1 className="text-2xl font-semibold tracking-tighter">
+            Welcome Back!
+          </h1>
+        </span>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-6">
+          {/* email field */}
+          <TextField.Root
+            placeholder="Enter your email"
+            type="email"
+            color="gray"
+            size="3"
+            variant="soft"
+            radius="large"
+            required
+          >
+            <TextField.Slot>
+              <EnvelopeClosedIcon height="16" width="16" />
+            </TextField.Slot>
+          </TextField.Root>
+          {/* password field */}
+          <TextField.Root
+            placeholder="Enter your password"
+            type="password"
+            color="gray"
+            size="3"
+            variant="soft"
+            radius="large"
+            required
+          >
+            <TextField.Slot>
+              <LockClosedIcon height="16" width="16" />
+            </TextField.Slot>
+          </TextField.Root>
+          {/* submit button */}
+          <Button
+            color="gray"
+            size="3"
+            variant="classic"
+            radius="large"
+            highContrast
+            className="hover:cursor-pointer"
+          >
+            Login
+          </Button>
+        </form>
+      </div>
     </section>
   );
 }
