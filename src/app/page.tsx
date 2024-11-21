20789ebe35f5ed4787ce4417cbf94a8974abea0a
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-21 18:20:04 +0600

    feat: initialized hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
-import { Button } from "@radix-ui/themes";
+import Hero from "@/components/hero";
 
 export default function HomePage() {
   return (
-    <div className="p-20">
-      <h1>Hello Wolrd</h1>
-      <Button
-        color="gray"
-        size="3"
-        variant="classic"
-        highContrast
-        radius="large"
-      >
-        Login
-      </Button>
-    </div>
+    <section>
+      <Hero />
+    </section>
   );
 }
---
+++ src/components/hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="w-full max-w-screen-lg mx-auto px-4 pt-40 pb-20">
+      <h1 className="text-3xl font-semibold tracking-tighter text-center">
+        Modern authentication system in Next.js
+      </h1>
+    </div>
+  );
+}
