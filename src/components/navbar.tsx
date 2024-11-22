11963ec01347ad3708ea95a957987110cf425482
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-22 22:53:44 +0600

    feat: conditionally show login button in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,26 +1,45 @@
+import { auth } from "@/auth";
 import { StackIcon } from "@radix-ui/react-icons";
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
 
-export default function Navbar() {
+export default async function Navbar() {
+  // read session in server component
+  const session = await auth();
+
   return (
     <nav className="w-full fixed top-0 h-16 flex items-center bg-white border-b border-b-gray-200">
       <div className="w-full max-w-screen-lg mx-auto px-4 flex items-center justify-between gap-6">
         <Link href="/">
           <StackIcon height="32" width="32" />
         </Link>
-        <Link href="/login">
-          <Button
-            color="gray"
-            size="3"
-            variant="classic"
-            radius="large"
-            highContrast
-            className="hover:cursor-pointer"
-          >
-            Login
-          </Button>
-        </Link>
+        {session?.user ? (
+          <Link href="/dashboard">
+            <Button
+              color="gray"
+              size="3"
+              variant="classic"
+              radius="large"
+              highContrast
+              className="hover:cursor-pointer"
+            >
+              Dashboard
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/login">
+            <Button
+              color="gray"
+              size="3"
+              variant="classic"
+              radius="large"
+              highContrast
+              className="hover:cursor-pointer"
+            >
+              Login
+            </Button>
+          </Link>
+        )}
       </div>
     </nav>
   );
--- src/components/pages/dashboard.tsx
+++ src/components/pages/dashboard.tsx
@@ -1,9 +1,29 @@
+import { signOut } from "@/auth";
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
-export default function Dashboard() {
+export default async function Dashboard() {
   return (
     <section className="w-full max-w-screen-lg mx-auto px-4 pt-32 pb-16">
       Dashboard
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <Button
+          color="red"
+          size="3"
+          variant="classic"
+          radius="large"
+          highContrast
+          className="hover:cursor-pointer"
+          type="submit"
+        >
+          Logout
+        </Button>
+      </form>
     </section>
   );
 }
