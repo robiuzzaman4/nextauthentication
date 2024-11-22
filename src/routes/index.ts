bddf323d4689127b8be18a295a5fcb1aa6673c07
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-22 21:24:04 +0600

    feat: define routes

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+export const AUTH_ROUTES = ["/login"]; // maybe you would add another route called '/register' here
+
+export const PUBLIC_ROUTES = ["/"]; // all public routes goes here
+
+export const API_AUTH_PREFIX = "/api/auth"; // related to next-auth api routes
+
+export const DEFAULT_LOGIN_REDIRECT = "/dashboard"; // default redirect after login
