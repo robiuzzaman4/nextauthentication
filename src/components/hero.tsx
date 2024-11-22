f8bfc169284b8b8d8b8ba2078d7f005c3a681654
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-22 15:21:22 +0600

    feat: hero left section design done

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,53 @@
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 import React from "react";
+
+// const TECH_STACKS = [
+//   {
+//     label: "Next.js",
+//     imgSrc: "/icons/next.svg",
+//   },
+//   {
+//     label: "TypeScript",
+//     imgSrc: "/icons/ts.svg",
+//   },
+//   {
+//     label: "Next Auth",
+//     imgSrc: "/icons/nextauth.png",
+//   },
+//   {
+//     label: "Radix UI",
+//     imgSrc: "/icons/radix.png",
+//   },
+//   {
+//     label: "Tailwind CSS",
+//     imgSrc: "/icons/tailwindcss.svg",
+//   },
+// ];
 
 export default function Hero() {
   return (
-    <div className="w-full max-w-screen-lg mx-auto px-4 pt-40 pb-20">
-      <h1 className="text-3xl font-semibold tracking-tighter text-center">
-        Modern authentication system in Next.js
-      </h1>
+    <div className="h-[calc(100vh-64px)] w-full max-w-screen-lg mx-auto px-4 pt-40 pb-20 grid md:grid-cols-2 gap-6 place-items-center">
+      <div className="flex flex-col gap-6">
+        <h1 className="text-3xl md:text-4xl font-semibold tracking-tighter">
+          RBAC authentication system in Next.js application.
+        </h1>
+        <p className="text-base font-normal text-gray-500 tracking-tighter">
+          An example of Role-based access control authentication system in
+          Next.js application with NextAuth (credential based) and TypeScript.
+        </p>
+        <Button
+          color="gray"
+          size="3"
+          variant="classic"
+          radius="large"
+          highContrast
+          className="hover:cursor-pointer w-full md:w-max"
+          asChild
+        >
+          <Link href="/login">Try Now</Link>
+        </Button>
+      </div>
     </div>
   );
 }
