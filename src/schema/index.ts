98fb0f0645f1c2f37bc440b494f4d362928a4749
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-22 20:54:49 +0600

    add login schema

---
+++ src/schema/index.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const LoginSchema = z.object({
+  email: z.string().email({
+    message: "Enter a valid email address.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters.",
+  }),
+});
