73e0c9218e9aad0e748dae7df3e4a34826dbc3d4
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-22 15:54:17 +0600

    feat: hero section right part design doen

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Code } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 
@@ -25,9 +25,15 @@ import React from "react";
 //   },
 // ];
 
+const CREDENTIALS = {
+  role: "'user' | 'admin' | 'client'",
+  email: "'demo@${role}.com'",
+  password: "'123456'",
+};
+
 export default function Hero() {
   return (
-    <div className="h-[calc(100vh-64px)] w-full max-w-screen-lg mx-auto px-4 pt-40 pb-20 grid md:grid-cols-2 gap-6 place-items-center">
+    <div className="min-h-[calc(100vh-64px)] pt-32 pb-16 w-full max-w-screen-lg mx-auto px-4 grid md:grid-cols-2 gap-12 place-items-center">
       <div className="flex flex-col gap-6">
         <h1 className="text-3xl md:text-4xl font-semibold tracking-tighter">
           RBAC authentication system in Next.js application.
@@ -47,6 +53,25 @@ export default function Hero() {
         >
           <Link href="/login">Try Now</Link>
         </Button>
+      </div>
+      <div className="bg-gray-100 border border-gray-200 p-2 w-full max-w-sm mx-auto rounded-3xl">
+        <div className="w-full flex flex-col gap-6 bg-white p-6 shadow-md rounded-2xl border border-gray-200">
+          <h4 className="text-sm font-semibold tracking-tighter">
+            Demo Credentials:
+          </h4>
+
+          <div className="grid gap-2">
+            <Code size="3" className="px-4 rounded-md">
+              role: {CREDENTIALS.role}
+            </Code>
+            <Code size="3" className="px-4 rounded-md">
+              email: {CREDENTIALS.email}
+            </Code>
+            <Code size="3" className="px-4 rounded-md">
+              password: {CREDENTIALS.password}
+            </Code>
+          </div>
+        </div>
       </div>
     </div>
   );
