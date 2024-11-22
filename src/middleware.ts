7b91a5f23096258eb11d8f05271624ed6fcf4070
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-22 21:52:18 +0600

    feat: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -41,10 +41,12 @@ export default auth(async (req) => {
   return;
 });
 
-// route mathcher
+// route mathcher [note: collected from 'https://clerk.com/docs/quickstarts/nextjs#add-clerk-middleware-to-your-app']
 export const config = {
   matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
