35087dc5be919070f0ccf478f4db629c08d5d682
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-11-23 20:54:02 +0600

    feat: update anatomy card

--- src/components/anatomy-card.tsx
+++ src/components/anatomy-card.tsx
@@ -16,22 +16,48 @@ export default function AnatomyCard({ page }: AnatomyCardProps) {
           ? "This Session Accessed via Client Component with Client-Side Rendering."
           : "This Session Accessed via Server Component with Server-Side Rendering."}
       </Code>
-      <Button
-        color="gray"
-        size="3"
-        variant="classic"
-        radius="large"
-        highContrast
-        className="hover:cursor-pointer w-full md:w-max text-sm"
-        asChild
-      >
-        <Link
-          href="https://github.com/robiuzzaman4/nextauthentication"
-          target="_blank"
+      <div className="w-full grid grid-cols-2 gap-2">
+        <Button
+          color="gray"
+          size="3"
+          variant="classic"
+          radius="large"
+          highContrast
+          className="hover:cursor-pointer w-full text-sm"
+          asChild
         >
-          <GitHubLogoIcon className="size-4" /> View on Github
-        </Link>
-      </Button>
+          {page === "csr" ? (
+            <Link
+              href="https://github.com/robiuzzaman4/nextauthentication"
+              target="_blank"
+            >
+              <GitHubLogoIcon className="size-4" /> View on Github
+            </Link>
+          ) : (
+            <Link
+              href="https://github.com/robiuzzaman4/nextauthentication"
+              target="_blank"
+            >
+              <GitHubLogoIcon className="size-4" /> View on Github
+            </Link>
+          )}
+        </Button>
+        <Button
+          color="gray"
+          size="3"
+          variant="classic"
+          radius="large"
+          highContrast
+          className="hover:cursor-pointer w-full text-sm"
+          asChild
+        >
+          {page === "csr" ? (
+            <Link href="/dashboard/ssr">Try SSR</Link>
+          ) : (
+            <Link href="/dashboard/csr">Try CSR</Link>
+          )}
+        </Button>
+      </div>
     </Card>
   );
 }
